c19436d060c4957481347d10c4dc38b6b61e9963
Denise-E <denueichen@gmail.com>
2023-11-17 16:34:04 -0300

    First test POST book

---
+++ test/api_library.test.js
@@ -0,0 +1,31 @@
+import { expect } from "chai"
+import supertest from "supertest"
+import book_generator from './generator/book_generator.js'
+
+const request = supertest('http://127.0.0.1:8080')
+
+describe(' *** Test APIRestfull *** ', () => {
+    /* describe('GET', () => {
+        it('debería retornar un status 200', async () => {
+            const reponse = await request.get('/api/productos')
+            expect(reponse.status).to.eql(200)
+        })
+    }) */
+
+    describe('POST', () => {
+        it('* Incorporación exitosa de un nuevo libro', async () => {
+            const producto = book_generator.get_book()
+            console.log(producto)
+
+            const response = await request.post('/api/books').send(producto)
+            expect(response.status).to.eql(200) // 201
+
+            const prodGuardado = response.body
+            console.log(prodGuardado)
+            expect(prodGuardado).to.include.keys('title','author','stock')
+            expect(prodGuardado.title).to.eql(producto.title)
+            expect(prodGuardado.author).to.eql(producto.author)
+            expect(prodGuardado.stock).to.eql(producto.stock)
+        })
+    })
+})
---
+++ test/generator/book_generator.js
@@ -0,0 +1,20 @@
+import { faker } from "@faker-js/faker/locale/en";
+
+const get_book = (_) => ({
+  _id: faker.string.uuid(),
+  title: generate_book_title(),
+  author: faker.person.fullName(),
+  stock: faker.number.int({ min: 0, max: 50 }),
+});
+
+const generate_book_title = (_) => {
+  const title = faker.word.words({ count: { min: 2, max: 5 } });
+  let titile_formatted = title.charAt(0).toUpperCase() + title.slice(1);
+  return titile_formatted;
+};
+
+//console.log(get_book())
+
+export default {
+  get_book,
+};
